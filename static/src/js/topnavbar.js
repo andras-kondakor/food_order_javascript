46b6084ee301a15cd825d2550274a1ed1d812c36
kondakor <kondakor2929@gmail.com>
2018-09-25 13:24:11 +0200

    navbar css in seperate file

--- static/src/js/topnavbar.js
+++ static/src/js/topnavbar.js
@@ -1,5 +1,5 @@
 document.getElementById("navMenu").innerHTML =
-'<nav class="navbar navbar-default">'+
+'<nav class="navbar navbar" style="background-color: firebrick;">'+
   '<div class="container">'+
     '<div class="navbar-header">'+
       '<a class="navbar-brand" href="#">'+
@@ -7,8 +7,8 @@ document.getElementById("navMenu").innerHTML =
       '</a>'+       
     '</div>'+
     '<ul class="nav navbar-nav navbar-right">'+
-      '<li class="active"><a href="#">Home</a></li>'+
-      '<li><a href="login.html">Exit</a></li>'+
+      '<li class="navbartext" class="active"><a href="#">Home</a></li>'+
+      '<li><a "href="login.html">Exit</a></li>'+
     '</ul>'+
   '</div>'+
 '</nav>';
